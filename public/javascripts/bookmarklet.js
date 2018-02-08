6aaee5798fedfb836dbab71b3ca0f94daad16d4a
docucharles <charles.johnston@docusign.com>
2018-02-08 14:10:48 -0800

    bookmarket to gihub hosting

--- public/javascripts/bookmarklet.js
+++ public/javascripts/bookmarklet.js
@@ -2,6 +2,6 @@ javascript: ! function() {
 	var head= document.getElementsByTagName('head')[0];
 	  var script= document.createElement('script');
 	  script.type= 'text/javascript';
-	  script.src= 'https://www.docusign.com/sites/default/files/cpn/372471.js?p3u62q123';
+	  script.src= 'https://docucharles.github.io/hackathon-header/public/javascripts/headerTemplate.js';
 	  head.appendChild(script);
  }();
