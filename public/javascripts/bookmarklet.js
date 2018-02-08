683d591d46ae282b1fdf51ef031efe9ac84eace6
docucharles <charles.johnston@docusign.com>
2018-02-08 13:48:31 -0800

    bookmarklet

---
+++ public/javascripts/bookmarklet.js
@@ -0,0 +1,7 @@
+javascript: ! function() {
+	var head= document.getElementsByTagName('head')[0];
+	  var script= document.createElement('script');
+	  script.type= 'text/javascript';
+	  script.src= 'https://www.docusign.com/sites/default/files/cpn/372471.js?p3u62q123';
+	  head.appendChild(script);
+ }();
